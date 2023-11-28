880be490b61b41aefb3288c4ba45512b8ca960e0
mayaassem5 <mayaassem5@gmail.com>
2023-11-28 09:06:40 -0800

    remove negative

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     digits_str = str(number)
-    digit = digits_str[-1] if number >= 0 else "-" + digits_str[-1]
+    digit = digits_str[-1]
     return int(digit*2)
