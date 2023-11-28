03430ccfc4b14e0afb9b4e2ca9d7ed76f4f0059d
mayaassem5 <mayaassem5@gmail.com>
2023-11-28 06:56:56 -0800

    edit pycodestyle

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for alpha in range(ord('a'), ord('z') + 1):
     if chr(alpha) == 'q' or chr(alpha) == 'e':
         continue
-    print("{}".format(chr(alpha)), end = "")
+    print("{}".format(chr(alpha)), end="")
