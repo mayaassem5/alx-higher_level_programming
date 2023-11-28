57e1a012e3ce7a26791f5110beb88d559fa6ec69
mayaassem5 <mayaassem5@gmail.com>
2023-11-28 02:05:36 -0800

    pycodestyle edit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
+
 def last(number):
     str_number = str(number)
 
     if number < 0:
-        return "-" +  str_number[-1]
+        return "-" + str_number[-1]
     else:
         return str_number[-1]
 
+
 last_num = last(number)
 intege = int(last_num)
+
 
 def state(intege):
     if intege > 5:
@@ -19,6 +23,7 @@ def state(intege):
         return " and is 0"
     else:
         return " and is less than 6 and not 0"
+
 
 num_state = state(intege)
 
