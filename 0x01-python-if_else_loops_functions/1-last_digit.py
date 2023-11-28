7d22f2709816f50d9e94d406fa052ebbea63c252
mayaassem5 <mayaassem5@gmail.com>
2023-11-28 01:55:14 -0800

    edit print

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,6 @@ def last(number):
     for digit in str_number:
         if digit == str_number[-1]:
                 return digit
-                break
 
 last_num = last(number)
 intege = int(last_num)
@@ -16,9 +15,9 @@ def state(intege):
         return " and is greater than 5"
     elif intege == 0:
         return " and is 0"
-    elif intege < 6 and last_num != 0:
+    else:
         return " and is less than 6 and not 0"
 
 num_state = state(intege)
 
-print(f"Last digit of {number} is " + last_num + num_state)
+print(f"Last digit of {number} is {last_num}{num_state}")
