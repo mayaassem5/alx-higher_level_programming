5072e38965f76805f9b3482fbafbda9cc99f660b
mayaassem5 <mayaassem5@gmail.com>
2023-11-30 18:17:22 +0200

    calc

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+summ = add(a, b)
+sub = sub(a , b)
+mul = mul(a, b)
+div = div(a, b)
+
+print("{} + {} = {}".format(a, b, summ))
+print("{} - {} = {}".format(a, b, sub))
+print("{} * {} = {}".format(a, b, mul))
+print("{} / {} = {}".format(a, b, div))
