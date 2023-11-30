d8d51249ab0745a4aa11081089e70ae4d303badc
mayaassem5 <mayaassem5@gmail.com>
2023-11-30 23:17:09 +0200

    edit import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import sys
-length = len(sys.argv)
-if length == 1:
-    print("{} arguments.".format(length - 1))
-elif length == 2:
-    print("{} argument:".format(length - 1))
-    print("1: {}".format(sys.argv[1]))
-else:
-    print("{} arguments:".format(length - 1))
+if __name__ == "__main__":
+    length = len(sys.argv)
+    if length == 1:
+        print("{} arguments.".format(length - 1))
+    elif length == 2:
+        print("{} argument:".format(length - 1))
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(length - 1))
 
-    for arg in range(1,length):
-        print("{}: {}".format(arg, sys.argv[arg]))
+        for arg in range(1,length):
+            print("{}: {}".format(arg, sys.argv[arg]))
