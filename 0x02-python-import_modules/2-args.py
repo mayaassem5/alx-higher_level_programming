18083d3d6ea7d7f73ed264304bba4729deb08aef
mayaassem5 <mayaassem5@gmail.com>
2023-11-30 23:14:47 +0200

    edit 1 condition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,9 @@ import sys
 length = len(sys.argv)
 if length == 1:
     print("{} arguments.".format(length - 1))
+elif length == 2:
+    print("{} argument:".format(length - 1))
+    print("1: {}".format(sys.argv[1]))
 else:
     print("{} arguments:".format(length - 1))
 
