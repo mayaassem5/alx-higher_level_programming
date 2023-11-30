75941bcab39f6f3a550a9f98455ae15c3ad254db
mayaassem5 <mayaassem5@gmail.com>
2023-12-01 00:58:56 +0200

    edit pycodestyle

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,19 +3,22 @@ from calculator_1 import add, sub, mul, div
 import sys
 if __name__ == "__main__":
     length = len(sys.argv)
+    t = sys.argv[2]
+    o = sys.argv[1]
+    th = sys.argv[3]
     if length < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-        if sys.argv[2] != "+" and sys.argv[2] != "/" and sys.argv[2] != "*" and sys.argv[2] != "-":
+        if t != "+" or t != "/" or t != "*" or t != "-":
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
         else:
-            if sys.argv[2] == "+":
-                print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], add(int(sys.argv[1]), int(sys.argv[3]))))
-            elif sys.argv[2] == "-":
-                print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], sub(int(sys.argv[1]), int(sys.argv[3]))))
-            elif sys.argv[2] == "/":
-                print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], div(int(sys.argv[1]), int(sys.argv[3]))))
+            if t == "+":
+                print("{} {} {} = {}".format(o, t, th, add(int(o), int(th))))
+            elif t == "-":
+                print("{} {} {} = {}".format(o, t, th, sub(int(o), int(th))))
+            elif t == "/":
+                print("{} {} {} = {}".format(o, t, th, div(int(o), int(th))))
             else:
-                print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], mul(int(sys.argv[1]), int(sys.argv[3]))))
+                print("{} {} {} = {}".format(o, t, th, mul(int(o), int(th))))
