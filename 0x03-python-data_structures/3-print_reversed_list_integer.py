52b4db76a696418bbed0aff2d84d72ed0501feab
mayaassem5 <mayaassem5@gmail.com>
2023-12-04 22:15:23 +0200

    adding else condition

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
         pass
-
-    my_list.reverse()
-    for i in range(len(my_list)):
-        print("{}".format(my_list[i]))
+    else:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print("{}".format(my_list[i]))
