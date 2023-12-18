657699c40acbe6521d0f0127a87331bb3771ab3f
mayaassem5 <mayaassem5@gmail.com>
2023-12-18 16:06:26 +0200

    rep finally with else

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end='')
         except:
             break
-        finally:
+        else:
             count += 1
     print()
     return (count)
