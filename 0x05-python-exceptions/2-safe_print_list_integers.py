b910bfd06f517c43afff6590e537b8727031114e
mayaassem5 <mayaassem5@gmail.com>
2023-12-18 16:53:59 +0200

    edit

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
         except TypeError:
             continue
-        except:   # noqa: E722
+        except IndexError:
             break
         else:
             count += 1
