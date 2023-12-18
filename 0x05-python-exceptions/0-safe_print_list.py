9df22504788741183c4d05ddf38aac4bc2c3b32f
mayaassem5 <mayaassem5@gmail.com>
2023-12-18 16:05:24 +0200

    rem ;

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end='')
         except:
-            break;
+            break
         finally:
             count += 1
     print()
