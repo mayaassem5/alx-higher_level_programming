70214402653e1aa49bf62a32b625aa40094991fb
mayaassem5 <mayaassem5@gmail.com>
2023-12-19 21:34:38 +0200

    area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Define a class square."""
+
+
+class Square:
+    """Class Square that defines a square object
+    """
+
+    def __init__(self, size=0):
+        """Initialize method that stores the size of the square
+
+            Args:
+                param1 (int): size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Initialize method that stores the area of the square
+        """
+
+        return (size ** 2)
