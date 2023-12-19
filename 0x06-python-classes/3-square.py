c34b6c7b6e047c3b07f2b0484529d0dd6a206fa1
mayaassem5 <mayaassem5@gmail.com>
2023-12-19 21:35:25 +0200

    int

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,7 +17,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = int(size)
 
     def area(self):
         """Initialize method that stores the area of the square
